be7b67d2fe6bf5afb18df1dc44c3774a6e7b921a
Tom Autier <autiertom@gmail.com>
2025-10-24 19:52:40 +0200

    Initial Commit

---
+++ Promi/PromiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromiApp.swift
+//  Promi
+//
+//  Created by MACBOOKPRO on 24/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct PromiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
